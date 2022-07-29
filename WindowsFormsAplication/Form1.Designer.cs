cb1f2e5bcd9025da24647728d5257a93321c80e9
JonatasSilva01 <jhownsantossilva@gmail.com>
2022-07-29 16:49:36 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsAplication/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsAplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.botaoEnviar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(126, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Click += new System.EventHandler(this.NameLabel);
+            // 
+            // txtNome
+            // 
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNome.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNome.Location = new System.Drawing.Point(201, 85);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(222, 47);
+            this.txtNome.TabIndex = 1;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // botaoEnviar
+            // 
+            this.botaoEnviar.BackColor = System.Drawing.Color.DarkGray;
+            this.botaoEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.botaoEnviar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.botaoEnviar.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botaoEnviar.Location = new System.Drawing.Point(201, 151);
+            this.botaoEnviar.Name = "botaoEnviar";
+            this.botaoEnviar.Size = new System.Drawing.Size(222, 51);
+            this.botaoEnviar.TabIndex = 2;
+            this.botaoEnviar.Text = "enviar";
+            this.botaoEnviar.UseVisualStyleBackColor = false;
+            this.botaoEnviar.Click += new System.EventHandler(this.botaoEnviar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(201, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Qual é seu nome?";
+            this.label2.Click += new System.EventHandler(this.labelPergunta);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 306);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.botaoEnviar);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button botaoEnviar;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WindowsFormsAplication/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NameLabel(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPergunta(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botaoEnviar_Click(object sender, EventArgs e)
+        {
+            var nome = "Jonatas";
+            txtNome.Text = "Meu nome é " + nome + "\nBem vindo ao meu programa!";
+        }
+    }
+}
